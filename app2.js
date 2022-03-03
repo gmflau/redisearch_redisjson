a73dd10b84b3efaa245846ee43a47ebffbe7852c
gmflau <gmflau@gmail.com>
2022-03-03 10:59:16 -0800

    Updated app2.js

--- app2.js
+++ app2.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 async function redisJSONDemo () {
   try {
     var keyprefix = "profile::";
-    var profile_prefix = 'KKNg25mFm_Q0eOG0rf2R7'
+    var profile_prefix = 'KKNg25m@gmail.com'
 
     const client = createClient({
           url: 'redis://default:SceKGdmO9JwoDuUmEBv6QEWV6do5ADp2@redis-14897.c280.us-central1-2.gce.cloud.redislabs.com:14897'
@@ -18,7 +18,7 @@ async function redisJSONDemo () {
     await client.connect();
 
     for (let counter = 1; counter < 6; counter++) {
-      await client.json.set(keyprefix + counter, '$', { profileId: profile_prefix + counter }); 
+      await client.json.set(keyprefix + counter, '$', { profileId: (counter + profile_prefix).replace(/\./g, '\\.').replace(/\@/g, '\\@') }); 
       const value = await client.json.get(keyprefix + counter, {
       // JSON Path: .node = the element called 'node' at root level.
         path: '.profileId'
@@ -35,23 +35,12 @@ async function redisJSONDemo () {
        }
     );
 
-    console.log(
-      await client.ft.search('idx22', '@profileId:KKNg25mFm_Q0eOG0rf2R73')
-    );
+    var email_address_esc = "3KKNg25m@gmail.com".replace(/\./g, '\\.').replace(/\@/g, '\\@');
+    console.log(email_address_esc);    
 
-//    var obj = JSON.parse(client.ft.search('idx22', '@profileId:KKNg25mFm_Q0eOG0rf2R73'));
-    var result = client.ft.search('idx22', '@profileId:KKNg25mFm_Q0eOG0rf2R73');
-
-
-    for (const item in result) {
-      console.log(item)
-    }
-   
-
-
-    Object.entries(result).map(item => {
-       console.log(item)
-    })
+    var results = await client.ft.search('idx22', `@profileId:${email_address_esc}`);
+    console.log(results);
+    console.log(results.documents);
 
     await client.quit();
   } catch (e) {
